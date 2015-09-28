5aa47099aa560887553df7f41c90b002a56451d7
Paul Grever <paulgrever@gmail.com>
2015-09-28 13:13:31 -0600

    updated pacakge info and made contact info dynamic

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Reaction Ambassador - Ambassador brand referral program for Reaction Commerce',
   name: 'getoutfitted:reaction-ambassador',
-  version: '0.1.0',
+  version: '0.2.0',
   // git: "https://github.com/reactioncommerce/reaction-paypal.git"
 });
 
