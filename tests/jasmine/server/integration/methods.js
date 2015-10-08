b109ca12076b43d43c3b0ec187cf8a5884f2eb61
Paul Grever <paulgrever@gmail.com>
2015-10-07 20:45:20 -0600

    five server method tests are passing

--- tests/jasmine/server/integration/methods.js
+++ tests/jasmine/server/integration/methods.js
@@ -35,7 +35,7 @@ describe('getoutfitted:reaction-ambassador methods', function() {
       expect(ReactionCore.Collections.Accounts.update).toHaveBeenCalled();
     });
 
-    it('should not upddate accounts when userid is not equal to account id', function() {
+    it('should not upddate account when userid is not equal to account id', function() {
       account = Factory.create('account');
       spyOn(ReactionCore.Collections.Accounts, 'update');
       var accountId = account.userId;
@@ -51,6 +51,43 @@ describe('getoutfitted:reaction-ambassador methods', function() {
       expect( function () {
         return Meteor.call('addRefererToAccounts', accountId, mbsy, campaignId, mbsy_source, expirationDate);
       }).toThrow(new Meteor.Error(403, "Access Denied"));
+      expect(ReactionCore.Collections.Accounts.update).not.toHaveBeenCalled();
+    });
+
+    it('should not update account if accountid is not a string', function() {
+      account = Factory.create('account');
+      spyOn(ReactionCore.Collections.Accounts, 'update');
+      var accountId = 1234;
+      var mbsy = "A1234";
+      var campaignId = "12345";
+      var mbsy_source = "1234_1234_1234_1234";
+      var expireTime = new Date();
+      var time = expireTime.getTime();
+      time += 180 * 24* 60 * 60 * 1000;
+      expireTime.setTime(time);
+      var expirationDate = JSON.stringify(expireTime.toUTCString());
+      spyOn(Meteor, 'userId').and.returnValue(accountId);
+      expect( function () {
+        return Meteor.call('addRefererToAccounts', accountId, mbsy, campaignId, mbsy_source, expirationDate);
+      }).toThrow();
+      expect(ReactionCore.Collections.Accounts.update).not.toHaveBeenCalled();
+    });
+    it('should not update account if mbsy is not a string', function() {
+      account = Factory.create('account');
+      spyOn(ReactionCore.Collections.Accounts, 'update');
+      var accountId = account.userId;
+      var mbsy = 1234;
+      var campaignId = "12345";
+      var mbsy_source = "1234_1234_1234_1234";
+      var expireTime = new Date();
+      var time = expireTime.getTime();
+      time += 180 * 24* 60 * 60 * 1000;
+      expireTime.setTime(time);
+      var expirationDate = JSON.stringify(expireTime.toUTCString());
+      spyOn(Meteor, 'userId').and.returnValue(accountId);
+      expect( function () {
+        return Meteor.call('addRefererToAccounts', accountId, mbsy, campaignId, mbsy_source, expirationDate);
+      }).toThrow();
       expect(ReactionCore.Collections.Accounts.update).not.toHaveBeenCalled();
     });
   });
