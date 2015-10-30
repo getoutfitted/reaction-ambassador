689847f9bcae2cfb11b295d8ea9954696119c7fa
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2015-10-30 14:02:27 -0600

    updated reaction core version

--- package.js
+++ package.js
@@ -14,8 +14,8 @@ Package.onUse(function (api) {
   api.use('meteor-platform');
   api.use('less');
   api.use('http');
-  api.use('reactioncommerce:core@0.9.3');
-  api.use('reactioncommerce:reaction-accounts@1.2.0');
+  api.use('reactioncommerce:core@0.9.4');
+  api.use('reactioncommerce:reaction-accounts@1.5.2');
   api.use('cfs:standard-packages@0.5.9');
   api.use('cfs:storage-adapter@0.2.2');
   api.use('cfs:graphicsmagick@0.0.18');
@@ -59,7 +59,7 @@ Package.onTest(function (api) {
   api.use('velocity:console-reporter@0.1.3');
   api.use('http');
 
-  api.use('reactioncommerce:core@0.9.0');
+  api.use('reactioncommerce:core@0.9.4');
   api.use('reactioncommerce:bootstrap-theme');
   api.use('getoutfitted:reaction-ambassador');
   api.addFiles('tests/jasmine/server/integration/methods.js', 'server');
